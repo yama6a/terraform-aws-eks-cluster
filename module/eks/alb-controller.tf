c0ae1d899adace81019c792ce090f31972f45b6e
Yama <yama.samoila@nordnet.se>
2022-05-01 18:59:13 +0200

    finish TF alb setup

---
+++ module/eks/alb-controller.tf
@@ -0,0 +1,20 @@
+data "aws_eks_cluster" "cluster" { name = module.eks.cluster_id }
+data "aws_eks_cluster_auth" "cluster" { name = module.eks.cluster_id }
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    token                  = data.aws_eks_cluster_auth.cluster.token
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  }
+}
+
+module "lb-controller" {
+  source       = "registry.terraform.io/Young-ook/eks/aws//modules/lb-controller"
+  cluster_name = var.cluster_name
+  oidc         = tomap({
+    url = module.eks.oidc_provider
+    arn = module.eks.oidc_provider_arn
+  })
+  tags = var.tags
+}
